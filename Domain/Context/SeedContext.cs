bd3b3ab269e84b3fc71e25d2d71c7a5321ade46e
Emad-Sayed <emad_tttt522@hotmail.com>
2020-09-15 11:51:18 +0200

    status row in db

--- Domain/Context/SeedContext.cs
+++ Domain/Context/SeedContext.cs
@@ -128,7 +128,8 @@ namespace Domain.Context
             builder.Entity<TicketStatus>().HasData(
             new TicketStatus { Id = 1, NameEN = "Waiting" ,NameAR="انتظار" },
             new TicketStatus { Id = 2, NameEN = "Serving" ,NameAR="تخدم"},
-            new TicketStatus { Id = 3, NameEN = "Closed", NameAR = "مغلقة" });
+            new TicketStatus { Id = 3, NameEN = "Closed", NameAR = "مغلقة" },
+            //new TicketStatus { Id = 4, NameEN = "Missed", NameAR = "تخلفت" });
             #endregion
             #region Configuration
             builder.Entity<Configuration>().HasData(
